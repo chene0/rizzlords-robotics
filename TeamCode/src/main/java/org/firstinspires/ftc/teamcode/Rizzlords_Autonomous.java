e9de902213c4c9733870f75698cd3a0985e1202c
demotivate <50751626+demotivate@users.noreply.github.com>
2023-11-07 16:51:37 -0700

    safety backup

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Rizzlords_Autonomous.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Rizzlords_Autonomous.java
@@ -1,3 +1,24 @@
+/*
+ * Copyright (c) 2021 OpenFTC Team
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
 package org.firstinspires.ftc.teamcode;
 
 import android.graphics.Color;
@@ -12,6 +33,11 @@ import com.qualcomm.robotcore.util.RobotLog;
 import org.firstinspires.ftc.robotcore.external.JavaUtil;
 import org.firstinspires.ftc.robotcore.external.navigation.Rotation;
 import org.firstinspires.ftc.robotcore.external.hardware.camera.WebcamName;
+import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
+import org.firstinspires.ftc.robotcore.external.navigation.AxesOrder;
+import org.firstinspires.ftc.robotcore.external.navigation.AxesReference;
+import org.firstinspires.ftc.robotcore.external.navigation.Orientation;
+import org.firstinspires.ftc.vision.apriltag.AprilTagProcessor;
 import org.openftc.apriltag.AprilTagDetection;
 import org.openftc.easyopencv.OpenCvCamera;
 import org.openftc.easyopencv.OpenCvCameraFactory;
@@ -22,8 +48,9 @@ import java.util.ArrayList;
 
 @Autonomous(name = "Rizzlords Autonomous", group = "Rizzlords")
 public class Rizzlords_Autonomous extends LinearOpMode {
-//    OpenCvCamera camera;
+    OpenCvCamera camera;
     AprilTagDetectionPipeline aprilTagDetectionPipeline;
+    AprilTagProcessor myAprilTagProcessor;
 
     static final double FEET_PER_METER = 3.28084;
 
@@ -39,12 +66,10 @@ public class Rizzlords_Autonomous extends LinearOpMode {
     // UNITS ARE METERS
     double tagsize = 0.166;
 
-    //    int ID_TAG_OF_INTEREST = 18; // Tag ID 18 from the 36h11 family
-    int LEFT = 1;
-    int MIDDLE = 2;
-    int RIGHT = 3;
+    int ID_TAG_OF_INTEREST = 18; // Tag ID 18 from the 36h11 family
 
     AprilTagDetection tagOfInterest = null;
+
 
     //hardware
     //    private ColorSensor Coloursensor;
@@ -94,6 +119,9 @@ public class Rizzlords_Autonomous extends LinearOpMode {
 //            }
 //        });
 
+        // Create the AprilTag processor and assign it to a variable.
+        myAprilTagProcessor = AprilTagProcessor.easyCreateWithDefaults();
+
         telemetry.setMsTransmissionInterval(50);
         TopRight = hardwareMap.get(DcMotor.class, "Top Right");
         BottomRight = hardwareMap.get(DcMotor.class, "Bottom Right");
@@ -115,89 +143,6 @@ public class Rizzlords_Autonomous extends LinearOpMode {
         BottomLeft.setDirection(DcMotorSimple.Direction.REVERSE);
         TopLeft.setDirection(DcMotorSimple.Direction.REVERSE);
         HandControl();
-
-        // Put initialization blocks here.
-//        waitForStart();
-        /*
-         * The INIT-loop:
-         * This REPLACES waitForStart!
-         */
-//        while (!isStarted() && !isStopRequested())
-//        {
-//            ArrayList<AprilTagDetection> currentDetections = aprilTagDetectionPipeline.getLatestDetections();
-//
-//            if(currentDetections.size() != 0)
-//            {
-//                boolean tagFound = false;
-//
-//                for(AprilTagDetection tag : currentDetections)
-//                {
-//                    if(tag.id == LEFT || tag.id == MIDDLE || tag.id == RIGHT)
-//                    {
-//                        tagOfInterest = tag;
-//                        tagFound = true;
-//                        break;
-//                    }
-//                }
-//
-//                if(tagFound)
-//                {
-//                    telemetry.addLine("Tag of interest is in sight!\n\nLocation data:");
-//                    tagToTelemetry(tagOfInterest);
-//                }
-//                else
-//                {
-//                    telemetry.addLine("Don't see tag of interest :(");
-//
-//                    if(tagOfInterest == null)
-//                    {
-//                        telemetry.addLine("(The tag has never been seen)");
-//                    }
-//                    else
-//                    {
-//                        telemetry.addLine("\nBut we HAVE seen the tag before; last seen at:");
-//                        tagToTelemetry(tagOfInterest);
-//                    }
-//                }
-//
-//            }
-//            else
-//            {
-//                telemetry.addLine("Don't see tag of interest :(");
-//
-//                if(tagOfInterest == null)
-//                {
-//                    telemetry.addLine("(The tag has never been seen)");
-//                }
-//                else
-//                {
-//                    telemetry.addLine("\nBut we HAVE seen the tag before; last seen at:");
-//                    tagToTelemetry(tagOfInterest);
-//                }
-//
-//            }
-//
-//            telemetry.update();
-//            sleep(20);
-//        }
-//
-//        /*
-//         * The START command just came in: now work off the latest snapshot acquired
-//         * during the init loop.
-//         */
-//
-//        /* Update the telemetry */
-//        if(tagOfInterest != null)
-//        {
-//            telemetry.addLine("Tag snapshot:\n");
-//            tagToTelemetry(tagOfInterest);
-//            telemetry.update();
-//        }
-//        else
-//        {
-//            telemetry.addLine("No tag snapshot available, it was never sighted during the init loop :(");
-//            telemetry.update();
-//        }
 
         /* Actually do something useful */
         waitForStart();
@@ -418,14 +363,16 @@ public class Rizzlords_Autonomous extends LinearOpMode {
         BottomLeft.setPower(bottomLeft);
     }
 
-//    void tagToTelemetry(AprilTagDetection detection)
-//    {
-//        telemetry.addLine(String.format("\nDetected tag ID=%d", detection.id));
-//        telemetry.addLine(String.format("Translation X: %.2f feet", detection.pose.x*FEET_PER_METER));
-//        telemetry.addLine(String.format("Translation Y: %.2f feet", detection.pose.y*FEET_PER_METER));
-//        telemetry.addLine(String.format("Translation Z: %.2f feet", detection.pose.z*FEET_PER_METER));
-//        telemetry.addLine(String.format("Rotation Yaw: %.2f degrees", Math.toDegrees(detection.pose.yaw)));
-//        telemetry.addLine(String.format("Rotation Pitch: %.2f degrees", Math.toDegrees(detection.pose.pitch)));
-//        telemetry.addLine(String.format("Rotation Roll: %.2f degrees", Math.toDegrees(detection.pose.roll)));
-//    }
+    void tagToTelemetry(AprilTagDetection detection)
+    {
+        Orientation rot = Orientation.getOrientation(detection.pose.R, AxesReference.INTRINSIC, AxesOrder.YXZ, AngleUnit.DEGREES);
+
+        telemetry.addLine(String.format("\nDetected tag ID=%d", detection.id));
+        telemetry.addLine(String.format("Translation X: %.2f feet", detection.pose.x*FEET_PER_METER));
+        telemetry.addLine(String.format("Translation Y: %.2f feet", detection.pose.y*FEET_PER_METER));
+        telemetry.addLine(String.format("Translation Z: %.2f feet", detection.pose.z*FEET_PER_METER));
+        telemetry.addLine(String.format("Rotation Yaw: %.2f degrees", rot.firstAngle));
+        telemetry.addLine(String.format("Rotation Pitch: %.2f degrees", rot.secondAngle));
+        telemetry.addLine(String.format("Rotation Roll: %.2f degrees", rot.thirdAngle));
+    }
 }
